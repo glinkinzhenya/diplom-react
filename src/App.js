e2b34907c5dd582a630bc3b79594a2a92b233f33
glinkinzhenya <glinkinzhenya@gmail.com>
2023-03-15 18:32:23 +0200

    njnjnj

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './pages/Header';
 import Left from './pages/Left';
--- src/pages/UserItem.jsx
+++ src/pages/UserItem.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, styled, Box } from "@mui/material";
-import "./UserItem.css"
 // export default function UserItem(props) {
 // делаем деструктуризацию
 
